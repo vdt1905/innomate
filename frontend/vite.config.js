c8cd07c1e9fc482c1dc997c73c9460cd271f3984
Vansh <tandelvansh0511@gmail.com>
2025-08-06 15:11:49 +0530

    Fix Vite config and Tailwind setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  base:'/',
-  plugins: [React()],
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [react()],
+});
